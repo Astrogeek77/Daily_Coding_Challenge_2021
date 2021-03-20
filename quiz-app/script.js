877e791311809646fd37df54070a976bac8a77d8
Gautam Jain <astrogeek77@gmail.com>
2021-03-20 23:29:06 +0530

    Add files via upload

---
+++ quiz-app/script.js
@@ -0,0 +1,146 @@
+const quizData = [
+    {
+        question: "Which Language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "How Many sides do a circle have?",
+        a: "1",
+        b: "0",
+        c: "infinite",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Which of these is not a Programming Language?",
+        a: "PHP",
+        b: "Swift",
+        c: "Node.Js",
+        d: "Ruby",
+        correct: "c",
+    },
+    {
+        question: "Which of these is not applicable for Frontend Works",
+        a: "Bootstrap",
+        b: "Tailwind CSS",
+        c: "React",
+        d: "Material CSS",
+        correct: "c",
+    },
+    {
+        question: "Which of these is not a Data Structure",
+        a: "Array",
+        b: "Linked_List",
+        c: "Queue",
+        d: "Object",
+        correct: "d",
+    },
+    {
+        question: "Which of these is not a part of Standart Template Library(CPP)",
+        a: "Sets",
+        b: "Vectors",
+        c: "Array",
+        d: "Math",
+        correct: "d",
+    },
+    {
+        question: "Who was the inventor of Calculus?",
+        a: "Issac Newton",
+        b: "Stephan Hawking",
+        c: "Albert Einstein",
+        d: "Galileo Galilei",
+        correct: "a",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
