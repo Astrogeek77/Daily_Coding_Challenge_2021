2b2332eafa2e1b9d5f5f9af684bfe70c8e992bfa
Gautam Jain <astrogeek77@gmail.com>
2021-04-09 22:31:29 +0530

    Update script.js

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,15 @@
+document.getElementById('searchIcon').onclick = function() {
+  document.getElementById('search').style.display = 'block';
+  document.getElementById('clear').style.display = 'block';
+  document.getElementById('search').focus();
+  document.getElementById('searchIcon').style.display = 'none';
+}
+document.getElementById('clear').onclick = function() {
+  document.getElementById('searchIcon').style.display = 'block';
+  document.getElementById('search').style.display = 'none';
+  document.getElementById('clear').style.display = 'none';
+}
+
 // Api config
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
